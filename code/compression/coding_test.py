8b4dab1a7c8a224fffed43a0c96f74d6ee6e26f0
Gergely Flamich <gf332@cam.ac.uk>
2019-07-03 21:37:08 +0100

    Created faster arithmetic coder

---
+++ code/compression/coding_test.py
@@ -0,0 +1,47 @@
+import time
+
+import numpy as np
+
+from coding import ArithmeticCoder, write_bin_code, read_bin_code
+
+
+num_symbols = 2**16
+message_length = 2000000
+test_file_path = "scratch_compression_test.miracle"
+
+P = np.ones(num_symbols + 1, dtype=np.int32)
+P[1:] = np.random.choice(1000, size=num_symbols) + 1
+
+message = np.zeros(message_length, dtype=np.int32)
+
+message[:-1] = np.random.choice(num_symbols, size=message_length - 1) + 1
+
+ac = ArithmeticCoder(P, precision=32)
+
+start = time.time()
+
+print("Coding..")
+code = ac.encode(message)
+
+print("Coded in {:.4f}s".format(time.time() - start))
+
+write_bin_code(''.join(code), test_file_path)
+
+code = read_bin_code(test_file_path)
+
+# start = time.time()
+
+# print("Decoding...")
+# decompressed = ac.decode(code)
+# print("Decoded in {:.4f}s".format(time.time() - start))
+
+
+# print(np.all(decompressed == message))
+
+start = time.time()
+
+print("Fast Decoding...")
+decompressed = ac.decode_fast(code)
+print("Decoded in {:.4f}s".format(time.time() - start))
+
+print(np.all(decompressed == message))
