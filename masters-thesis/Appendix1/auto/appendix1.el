7b3c42a8541bc081269e704be32d775424e83a16
Gergely Flamich <gf332@cam.ac.uk>
2019-08-16 11:39:18 +0100

    Some cleanup

--- masters-thesis/Appendix1/auto/appendix1.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "appendix1"
- (lambda ()
-   (LaTeX-add-labels
-    "chapter:appendix_a"
-    "alg:harsha_rej_sampling"
-    "alg:miracle_imp_samp"))
- :latex)
-
--- masters-thesis/Appendix2/auto/appendix2.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "appendix2"
- (lambda ()
-   (LaTeX-add-labels
-    "chapter:appendix_b"
-    "tab:beta_layout"))
- :latex)
-
